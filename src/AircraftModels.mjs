dd5f0b126847e9df3ba2233613c8b6f94c32a059
Liz3 <accs@liz3.net>
2025-10-11 00:25:38 +0200

    update model matching

--- src/AircraftModels.mjs
+++ src/AircraftModels.mjs
@@ -1,24 +1,20 @@
 const models = {
-  "Asobo Cessna Citation Longitude": "C700",
-  "Cessna Longitude Asobo": "C700",
-  "Microsoft Vision Jet": "SF50",
-  "Asobo TBM 930": "TMB9",
-  "Citation CJ3+": "C25B",
+  "Longitude": "C700",
+  "Vision Jet": "SF50",
+  "TBM 930": "TMB9",
+  "CJ3+": "C25B",
 };
 const weights = {
-  "Asobo Cessna Citation Longitude": {
+  "Longitude": {
     pax: [3, 5, 4, 6, 7, 9, 8, 10],
     cargo: [11, 12],
   },
-  "Cessna Longitude Asobo": {
-    pax: [3, 5, 4, 6, 7, 9, 8, 10],
-    cargo: [11, 12],
-  },
-  "Asobo TBM 930": {
+
+  "TBM 930": {
     pax: [2, 3, 4, 5, 6],
     cargo: [7, 8],
   },
-  "Citation CJ3+": {
+  "CJ3+": {
     pax: [1,2,3,4,5,6,7,8.9],
     cargo: [10, 11],
   },
